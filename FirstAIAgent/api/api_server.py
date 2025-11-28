cef23d1e878d67f848b37b3cfa5f88f125d58ab9
Will Hager <wbhager@gmail.com>
2025-11-28 12:45:08 -0500

    finished up base api_server.py code

--- FirstAIAgent/api/api_server.py
+++ FirstAIAgent/api/api_server.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from anthropic import Anthropic
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-client = Anthropic(api_key = os.getenv("ANT_AP_KY"))
+from logic.ai_response_generator import client
 
 app = FastAPI()
+
+class Message(BaseModel):
+    message: str
+
+#Creating api endpoint,
+##### Note: user represents user talking to model, assistant represents referencing previous prompt responses, system is system prompts
+@app.post("/chat")
+async def chat_endpoint(data: Message):
+    user_message = data.message
+
+    response = client.messages.create(
+        model = "claude-sonnet-4-5-20250929",
+        max_tokens = 300,
+        messages = [{"role": "user", "content": user_message}]
+    )
+
+    reply = response.content[0].text
+
+    return reply
+
+
