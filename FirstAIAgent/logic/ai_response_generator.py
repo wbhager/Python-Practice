494fbf29d4ae8f157a638c498e392f08db3dbb5d
Will Hager <wbhager@gmail.com>
2025-12-08 09:59:53 -0500

    added temperature field

--- FirstAIAgent/logic/ai_response_generator.py
+++ FirstAIAgent/logic/ai_response_generator.py
@@ -14,6 +14,7 @@ def get_claude_response(user_message: str):
         model = "claude-sonnet-4-5-20250929",
         system = SYSTEM_PROMPT,
         max_tokens = 300,
+        temperature = 0.7,
         messages = [{"role": "user", "content": user_message}])
 
     return response.content[0].text
