189d7fadf6bd06ec48dc69741da607030ca574a5
Will Hager <wbhager@gmail.com>
2025-12-14 16:30:22 -0500

    added basic modification permissions for google calendar in scopes

--- FirstAIAgent/api/tools.py
+++ FirstAIAgent/api/tools.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
 app = FastAPI()
 
 @app.post("/gcal/add_event")
